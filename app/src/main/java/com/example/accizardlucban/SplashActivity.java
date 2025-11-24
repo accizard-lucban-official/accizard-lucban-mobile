e836827f7bf6d0adaddb382217ab12c44f95b413
Ashley Shane Corpuz <corpuzashleyshane@gmail.com>
2025-11-25 04:05:17 +0800

    lala

--- app/src/main/java/com/example/accizardlucban/SplashActivity.java
+++ app/src/main/java/com/example/accizardlucban/SplashActivity.java
@@ -11,7 +11,7 @@ import com.airbnb.lottie.LottieAnimationView;
 public class SplashActivity extends AppCompatActivity {
     
     private static final String TAG = "SplashActivity";
-    private static final int SPLASH_DURATION = 4000; // 4 seconds
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
